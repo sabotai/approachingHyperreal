50c9a0f6f7610db5118f847e8dc5c5ecb88dee04
sabotai <sabotaione@gmail.com>
2015-02-12 18:17:03 -0500

    scripted fade in effect for dofs and eden

---
+++ Assets/Scripts/fadeIn.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class fadeIn : MonoBehaviour {
+
+	public string scriptTitle;
+
+	private Component dofs, eden; 
+	private FieldInfo field1, field1A;
+	private FieldInfo field2, field2A;
+	private float f1Pos, f1PosA;
+	private float f2Pos, f2PosA;
+	private bool upDownSwitch;
+	private float lowThresh, highThresh, increment;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		Component[] components = GameObject.Find("RightEyeAnchor").GetComponents<Component>();
+		foreach (Component c in components) {
+			Debug.Log(c.GetType());
+		}
+
+		*/
+
+		dofs = GameObject.Find("RightEyeAnchor").GetComponent<DepthOfFieldScatter>();
+		field1 = dofs.GetType().GetField("focalLength");
+		field2 = dofs.GetType().GetField("aperture");
+		f1Pos = 3f;
+		f2Pos = 14f;
+
+		f1PosA = 0f;
+		f2PosA = 5f;
+		
+		lowThresh = 0.17f;
+		highThresh = 3f;
+		increment = 0.01f;
+		upDownSwitch = false;
+
+
+		
+		eden = GameObject.Find("RightEyeAnchor").GetComponent<EdgeDetectEffectNormals>();
+		field1A = eden.GetType().GetField("edgesOnly");
+		field2A = eden.GetType().GetField ("luminanceThreshold");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((f1Pos >= lowThresh) && (f1Pos <= highThresh)) {
+			Debug.Log ("increase pos");
+			field1.SetValue (dofs, f1Pos);
+			field2.SetValue (dofs, f2Pos);
+			f1Pos -= increment;
+			if (f2Pos < 26.6){
+				f2Pos += increment*10f;}
+		}
+		if (f1PosA < 1) {
+			
+			field1A.SetValue (eden, f1PosA);
+			f1PosA+=0.01f;		
+		}
+		if (f2PosA > 0.0001f){
+			field2A.SetValue (eden, f2PosA);
+			f2PosA -= 0.01f;
+		}
+	}
+
+}
