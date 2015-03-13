40d7e5c7c62fd48638ae7aacbac3f5410c7553c0
sabotai <sabotaione@gmail.com>
2015-03-13 14:56:22 -0400

    added depth mask for 1208 from abstract scene

---
+++ Assets/Scripts/SetRenderQueueWithChildren.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[AddComponentMenu("Rendering/SetRenderQueueWithChildren")]
+
+public class SetRenderQueueWithChildren : MonoBehaviour {
+
+	 
+	public int queuePosition = 3000;
+		[SerializeField]
+		protected int[] m_queues = new int[]{3000};
+		
+		protected void Awake() {
+
+		/*
+			Material[] materials = renderer.materials;
+			for (int i = 0; i < materials.Length && i < m_queues.Length; ++i) {
+				materials[i].renderQueue = m_queues[i];
+			}
+		*/
+		
+		Renderer[] rendererz = gameObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer rend in rendererz) {
+			//rend.materials.renderQueue = queuePosition;
+			Debug.Log (rend.materials);
+			for (int i = 0; i < rend.materials.Length && i < m_queues.Length; ++i) {
+				rend.materials[i].renderQueue = m_queues[i];
+			}
+			}
+		}
+}
